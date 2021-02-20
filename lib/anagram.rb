5d7819f48a073f6dfac1b81de8faba1b16cccec3
Jason Whitlock <jrwhitlock7@gmail.com>
2021-02-20 12:19:04 -0700

    commit tree 7ef7df2a60cbab048f0fed6196c2db8e76cf9d5b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        #so when we want a word stored in an array, you can use .map. map returns a new array.
+        #to get an empty arr
+        @word = word
+    end
+
+    def match(word)
+        word.select {|i| i.split("").sort == @word.split("").sort }
+    end
+end
